94d3265771582bd80ef75ac97afad60411e74873
Rene Santiago <rsj510@gmail.com>
2022-12-05 08:24:54 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+const mongoose = require("mongoose")
+const app = express()
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(process.env.MONGO)
+mongoose.connection
+.on("open", () => console.log("Connected to Mongo"))
+.on("close", () => console.log("Disconnected to Mongo"))
+.on("error", () => console.log(error))
+
+app.use(morgan("dev"))
+app.use(methodOverride("method"))
+app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.send("<h1>Server is working</h1>")
+})
+
+app.listen(PORT, () => {
+    console.log(`You are now on Port: ${PORT}`)
+})
