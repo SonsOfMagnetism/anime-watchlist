213d5d19823f02746425fa93aa8718d56bb29905
Rene Santiago <rsj510@gmail.com>
2022-12-06 19:46:19 -0500

    created seed model and added seed script to package.json

---
+++ models/seed.js
@@ -0,0 +1,19 @@
+require("dotenv").config()
+const mongoose = require("./connection")
+const Anime = require("./anime")
+
+mongoose.connnection.on("open", () => {
+    const startAnimes = [
+        {title: "Dragon Ball", yearReleased: 1986, watchedEpisodes: 153, totalEpisodes: 153, completed: true},
+        {title: "My Hero Academia", yearReleased: 2016, watchedEpisodes: 109, totalEpisodes: 119, completed: false},
+        {title: "Mobile Suit Gundam", yearReleased: 1979, watchedEpisodes: 43, totalEpisodes:43, completed: true},
+        {title: "Samurai Champloo", yearReleased: 2004, watchedEpisodes: 24, totalEpisodes: 24, completed: true},
+        {title: "One Punch Man", yearReleased: 2015, watchedEpisodes: 24, totalEpisodes: 24, completed: true}
+    ]
+    Anime.remove({}, (err, data) => {
+        Anime.create(startAnimes, (err, data) => {
+            console.log(data)
+            mongoose.connection.close()
+        })
+    })
+})
