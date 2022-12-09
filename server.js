e42dd13953c347cfb4018bc2e24e37d51cd7de31
Rene Santiago <rsj510@gmail.com>
2022-12-09 07:54:24 -0500

    connect user router to server

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const methodOverride = require("method-override")
 const app = express()
 const PORT = process.env.PORT || 3000
 const AnimeRouter = require("./controllers/animeController")
+const UserRouter = require("./controllers/user")
 
 app.use(morgan("dev"))
 app.use(methodOverride("_method"))
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
 app.use("/anime", AnimeRouter)
+app.use("/user", UserRouter)
 
 app.listen(PORT, () => {
     console.log(`You are now on Port: ${PORT}`)
