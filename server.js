dd9e25f8ea5a50635ed14478d1428bc719aac5ca
Rene Santiago <rsj510@gmail.com>
2022-12-10 10:52:52 -0500

    configuring sessions middleware

--- server.js
+++ server.js
@@ -6,11 +6,19 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const AnimeRouter = require("./controllers/animeController")
 const UserRouter = require("./controllers/user")
+const session = require("express-session")
+const MongoStore = require("connect-mongo")
 
 app.use(morgan("dev"))
 app.use(methodOverride("_method"))
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
+app.use(session({
+    secret: process.env.SECRET,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO }),
+    saveUninitialized: true,
+    resave: false
+}))
 app.use("/anime", AnimeRouter)
 app.use("/user", UserRouter)
 
